b6e9de8abfe9b304e5a54bc47dac4e8262875387
wra-sol <88047072+wra-sol@users.noreply.github.com>
2023-04-25 16:32:40 -0400

    Update firebaseConfig.js

--- client/src/firebaseConfig.js
+++ client/src/firebaseConfig.js
@@ -3,13 +3,13 @@ import {getDatabase} from 'firebase/database';
 import {getAuth} from 'firebase/auth';
 
 export const firebaseConfig = {
-  apiKey: 'AIzaSyDHaH3upBNJTvqrWDs1invZpxqVAt9CiyA',
-  authDomain: 'fern-walkthrough.firebaseapp.com',
-  projectId: 'fern-walkthrough',
-  storageBucket: 'fern-walkthrough.appspot.com',
-  messagingSenderId: '943562711269',
-  appId: '1:943562711269:web:a3f555407981f81716f40c',
-  databaseURL: 'https://fern-walkthrough-default-rtdb.firebaseio.com',
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
+  databaseURL: '',
 };
 
 const app = initializeApp (firebaseConfig);
