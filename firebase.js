ccae1d075beeeebe3b250ebbdef8950094265414
wra-sol <88047072+wra-sol@users.noreply.github.com>
2023-04-25 16:32:06 -0400

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,14 +3,14 @@ import {getDatabase} from 'firebase/database';
 import {getAuth} from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDHaH3upBNJTvqrWDs1invZpxqVAt9CiyA",
-  authDomain: "fern-walkthrough.firebaseapp.com",
-  databaseURL: "https://fern-walkthrough-default-rtdb.firebaseio.com",
-  projectId: "fern-walkthrough",
-  storageBucket: "fern-walkthrough.appspot.com",
-  messagingSenderId: "943562711269",
-  appId: "1:943562711269:web:a3f555407981f81716f40c",
-  measurementId: "G-GXW3S5P7MN"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: ""
+  projectId: "fern",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 const firebase = initializeApp (firebaseConfig);
